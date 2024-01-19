56d34734661ea3dc538a6abc074757d9c4bc8d1a
rezakusuma40 <110301530+rezakusuma40@users.noreply.github.com>
2024-01-19 10:52:37 +0700

    add all tugas pertama harisenin

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},    
+    screen: {
+        xl: '1440px',
+      }
+  },
+  plugins: [],
+}
+
